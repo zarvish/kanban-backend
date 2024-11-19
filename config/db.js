084c6e3208f1e614c68c7b3301965d2a9ed7a406
vishnusony <vishnu.kant@deepdivemedia.in>
2024-11-19 22:14:58 +0530

    Added a static MongoDB URI as a fallback for development or testing environments.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
+// Function to connect to MongoDB
 const connectMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI ||"")
-        console.log('MongoDB connected');
+        // Attempt to connect to MongoDB using the connection string.
+        // First, it tries to use the environment variable 'MONGODB_URI' if defined.
+        // If 'MONGODB_URI' is not set (undefined), it falls back to a hardcoded static connection string.
+        await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://technicalstudent83:P9RfslqosvkqeGW1@cluster0.944lf.mongodb.net/kanban")
+        console.log('MongoDB connected');  // Log successful connection
     } catch (error) {
+        // If an error occurs during connection, log the error and stop the process.
         console.error('Error connecting to MongoDB', error);
-        process.exit(1); // Exit process with failure
+        process.exit(1); // Exit process with failure if the connection is unsuccessful
     }
 };
-export default connectMongoDB;
+
+export default connectMongoDB; // Export the function to use it elsewhere in the app
